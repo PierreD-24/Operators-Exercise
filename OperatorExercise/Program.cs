ed3071e0ef8ea547d4fc0c6d54736319f8ee80ea
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-09-19 20:39:21 -0700

    initial commit finished step one of exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}+{b} is {sum}. {a}-{b} is {difference}.");
+            Console.WriteLine($"{a}*{b} is {product}.");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
         }
     }
 }
